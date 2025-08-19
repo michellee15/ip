42937dcf8989c74fe11d679fa38370ec8a5a2dce
michellee15 <e1459250@u.nus.edu>
2025-08-19 13:30:33 +0800

    Level-0

---
+++ src/main/java/Jerry.java
@@ -0,0 +1,10 @@
+public class Jerry {
+    public static void main(String[] args) {
+        System.out.println("___________________________________________________");
+        System.out.println("Hello, nice to meet you! I'm Jerry the mouse!");
+        System.out.println("What can I do for you today?");
+        System.out.println("___________________________________________________");
+        System.out.println("Bye! See you next time!");
+        System.out.println("___________________________________________________");
+    }
+}
