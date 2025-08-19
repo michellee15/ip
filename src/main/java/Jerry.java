3a1b98650a8c9db717c53ddeabba7d19353e3ba1
michellee15 <e1459250@u.nus.edu>
2025-08-19 14:07:06 +0800

    Level-2

--- src/main/java/Jerry.java
+++ src/main/java/Jerry.java
@@ -4,6 +4,8 @@ public class Jerry {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String userInput;
+        String[] tasks = new String[100];
+        int count = 0;
         System.out.println("___________________________________________________");
         System.out.println("Hello, nice to meet you! I'm Jerry the mouse!");
         System.out.println("What can I do for you today?");
@@ -15,8 +17,16 @@ public class Jerry {
                 System.out.println("Bye! See you next time :)");
                 System.out.println("___________________________________________________");
                 break;
+            } else if (userInput.toLowerCase().equals("list")) {
+                System.out.println("Your task list: ");
+                for (int i = 0; i < count; i++) {
+                    System.out.println((i + 1) + ". " + tasks[i]);
+                }
+                System.out.println("___________________________________________________");
             } else {
-                System.out.println(userInput);
+                tasks[count] = userInput;
+                count++;
+                System.out.println("new task added: " + userInput);
                 System.out.println("___________________________________________________");
             }
         }
