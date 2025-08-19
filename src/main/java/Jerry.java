66f0c90bbc40fc4f60f1741fa1c09a1ef2557bd6
michellee15 <e1459250@u.nus.edu>
2025-08-19 13:48:46 +0800

    Level-1

--- src/main/java/Jerry.java
+++ src/main/java/Jerry.java
@@ -1,10 +1,24 @@
+import java.util.Scanner;
+
 public class Jerry {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String userInput;
         System.out.println("___________________________________________________");
         System.out.println("Hello, nice to meet you! I'm Jerry the mouse!");
         System.out.println("What can I do for you today?");
         System.out.println("___________________________________________________");
-        System.out.println("Bye! See you next time!");
-        System.out.println("___________________________________________________");
+        while (true) {
+            userInput = sc.nextLine();
+            System.out.println("___________________________________________________");
+            if (userInput.toLowerCase().equals("bye")) {
+                System.out.println("Bye! See you next time :)");
+                System.out.println("___________________________________________________");
+                break;
+            } else {
+                System.out.println(userInput);
+                System.out.println("___________________________________________________");
+            }
+        }
     }
 }
