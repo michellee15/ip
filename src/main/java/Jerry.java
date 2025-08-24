b9527a8e524cb869c210a61315761d7c39f88d52
michellee15 <e1459250@u.nus.edu>
2025-08-24 14:11:22 +0800

    Improve formatting of syntax in main

--- src/main/java/Jerry.java
+++ src/main/java/Jerry.java
@@ -29,69 +29,69 @@ public class Jerry {
                             "bye/list/todo/deadline/event/mark/unmark/delete");
                 }
                 switch (command) {
-                    case BYE:
-                        System.out.println("Bye! See you next time :D");
+                case BYE:
+                    System.out.println("Bye! See you next time :D");
                         System.out.println("___________________________________________________");
                         return;
-                    case LIST:
-                        if (tasks.isEmpty()) {
-                            throw new JerryException("Your task list is currently empty...");
-                        }
-                        System.out.println("Your task list:");
-                        for (int i = 0; i < tasks.size(); i++) {
-                            System.out.println((i + 1) + ". " + tasks.get(i));
-                        }
-                        System.out.println("___________________________________________________");
-                        break;
-                    case DEADLINE:
-                        if (entries.length < 2) {
-                            throw new JerryException("You need to specify the task description and the due date");
-                        }
-                        Task deadline = new Deadline(userInput);
-                        tasks.add(deadline);
-                        System.out.println("Nice! This task has been added to the list:\n" + deadline);
-                        System.out.println("Now you have " + tasks.size() + " in your list :)");
-                        System.out.println("___________________________________________________");
-                        break;
-                    case EVENT:
-                        if (entries.length < 2) {
-                            throw new InvalidCommandFormatException("Uh Oh! You need to specify the event description and a timeframe (its starting time and ending time)");
-                        }
-                        Task event = new Events(userInput);
-                        tasks.add(event);
-                        System.out.println("Okay! I've added this to your task list:\n" + event);
-                        System.out.println("Now you have " + tasks.size() + " in your list :)");
-                        System.out.println("___________________________________________________");
-                        break;
-                    case TODO:
-                        if (entries.length < 2) {
-                            throw new InvalidCommandFormatException("Uh Oh! You forgot to describe what your todo is...");
-                        }
-                        Task todo = new ToDo(userInput);
-                        tasks.add(todo);
-                        System.out.println("Great! New task added: " + todo);
-                        System.out.println("Now you have " + tasks.size() + " in your list :)");
-                        System.out.println("___________________________________________________");
-                        break;
-                    case MARK:
-                        int markIdx = checkIndex(entries, tasks.size());
-                        tasks.get(markIdx).mark();
-                        System.out.println("Yay! One task down: " + tasks.get(markIdx));
-                        System.out.println("___________________________________________________");
-                        break;
-                    case UNMARK:
-                        int unmarkIdx = checkIndex(entries, tasks.size());
-                        tasks.get(unmarkIdx).unmark();
-                        System.out.println("Noted! I've marked this task as undone: " + tasks.get(unmarkIdx));
-                        System.out.println("___________________________________________________");
-                        break;
-                    case DELETE:
-                        int delIdx = checkIndex(entries, tasks.size());
-                        System.out.println("Noted! I've marked this task as deleted: " + tasks.get(delIdx));
-                        tasks.remove(delIdx);
-                        System.out.println("Now you have " + tasks.size() + " in your list :)");
-                        System.out.println("___________________________________________________");
-                        break;
+                case LIST:
+                    if (tasks.isEmpty()) {
+                        throw new JerryException("Your task list is currently empty...");
+                    }
+                    System.out.println("Your task list:");
+                    for (int i = 0; i < tasks.size(); i++) {
+                        System.out.println((i + 1) + ". " + tasks.get(i));
+                    }
+                    System.out.println("___________________________________________________");
+                    break;
+                case DEADLINE:
+                    if (entries.length < 2) {
+                        throw new JerryException("You need to specify the task description and the due date");
+                    }
+                    Task deadline = new Deadline(userInput);
+                    tasks.add(deadline);
+                    System.out.println("Nice! This task has been added to the list:\n" + deadline);
+                    System.out.println("Now you have " + tasks.size() + " in your list :)");
+                    System.out.println("___________________________________________________");
+                    break;
+                case EVENT:
+                    if (entries.length < 2) {
+                        throw new InvalidCommandFormatException("Uh Oh! You need to specify the event description and a timeframe (its starting time and ending time)");
+                    }
+                    Task event = new Events(userInput);
+                    tasks.add(event);
+                    System.out.println("Okay! I've added this to your task list:\n" + event);
+                    System.out.println("Now you have " + tasks.size() + " in your list :)");
+                    System.out.println("___________________________________________________");
+                    break;
+                case TODO:
+                    if (entries.length < 2) {
+                        throw new InvalidCommandFormatException("Uh Oh! You forgot to describe what your todo is...");
+                    }
+                    Task todo = new ToDo(userInput);
+                    tasks.add(todo);
+                    System.out.println("Great! New task added: " + todo);
+                    System.out.println("Now you have " + tasks.size() + " in your list :)");
+                    System.out.println("___________________________________________________");
+                    break;
+                case MARK:
+                    int markIdx = checkIndex(entries, tasks.size());
+                    tasks.get(markIdx).mark();
+                    System.out.println("Yay! One task down: " + tasks.get(markIdx));
+                    System.out.println("___________________________________________________");
+                    break;
+                case UNMARK:
+                    int unmarkIdx = checkIndex(entries, tasks.size());
+                    tasks.get(unmarkIdx).unmark();
+                    System.out.println("Noted! I've marked this task as undone: " + tasks.get(unmarkIdx));
+                    System.out.println("___________________________________________________");
+                    break;
+                case DELETE:
+                    int delIdx = checkIndex(entries, tasks.size());
+                    System.out.println("Noted! I've marked this task as deleted: " + tasks.get(delIdx));
+                    tasks.remove(delIdx);
+                    System.out.println("Now you have " + tasks.size() + " in your list :)");
+                    System.out.println("___________________________________________________");
+                    break;
 
                 }
             } catch (JerryException e) {
